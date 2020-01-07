5382ccfc0d707fd38981a77183a923ec01a71219
Ibrahim shamma <i.abushammah@gmail.com>
2020-01-07 15:29:47 +0200

    enhance test

--- api/schema.js
+++ api/schema.js
@@ -3,25 +3,33 @@ const Schema = mongoose.Schema;
 
 // collection and schema for Registration
 let Registration = new Schema({
-	first_name: {
-		type: String
-	},
-	last_name: {
-		type: String
-	},
-	user_name: {
-		type: String,
-		unique: true,
-		required: true,
-		trim: true
-	},
-	password: {
-		type: String,
-		required: true,
-		trim: true
-	}
-},{
-		collection: 'Registration'
+    first_name: {
+        type: String,
+        match: [/^[a-zA-Z0-9]+$/, 'is invalid'],
+        required: [true, "can't be blank"],
+
+    },
+    last_name: {
+        type: String,
+        match: [/^[a-zA-Z0-9]+$/, 'is invalid'],
+        required: [true, "can't be blank"]
+
+    },
+    user_name: {
+        type: String,
+        lowercase: true,
+        unique: true,
+        required: [true, "can't be blank"],
+        match: [/^[a-zA-Z0-9]+$/, 'is invalid'],
+        index: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    }
+}, {
+    collection: 'Registration'
 });
 
 module.exports = mongoose.model('Registration', Registration);
